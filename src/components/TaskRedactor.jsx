04526edb18f9a80f8217984e3542b7698144b7f8
Dekardexe <ddekax@yandex.ru>
2023-10-19 17:26:36 +0300

    добавлено описание, fix: редактирование

--- src/components/TaskRedactor.jsx
+++ src/components/TaskRedactor.jsx
@@ -1,54 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { VscSignIn, VscSave, VscCheck, VscTrash } from "react-icons/vsc";
+import { VscSignIn, VscSave, VscCheck, VscTrash, VscEdit } from "react-icons/vsc";
 
 const TaskRedactor = function (props) {
-   const [taskTitle, setTaskTitle] = useState();
-   const [edited, setEdited] = useState();
+   const [taskTitle, setTaskTitle] = useState(props.task.title);
 
    function thisTaskEditor() {
       props.task.title = taskTitle;
       props.onEditRender(props.task);
    }
 
-   function changeTest() {
-
-      setEdited(props.task.title)
-      console.log(edited)
-   }
-
-
-   //console.log('не в функции получили props edit',taskTitle)
    return (
       <div className='taskEditorModalWindow'>
          <div className='taskEditor'>
             <div >
-               <p className='greyZone'
-               onClick={function() {
-                  // setA(!a);
-                  // console.log(taskTitle)
-                  // let b = props.task.title;
-                  // setTaskTitle(b);
-               }}
-               >{props.task.title}</p>
-                <div className='greyZone' >
-                  <input maxLength={150} style={{ width: '96%', borderRadius: '3px', padding: '1% ', fontSize: '18px', backgroundColor: 'rgba(27, 54, 68, 0.8)', color: 'white' }}
-                     placeholder="Редактировать заметку"
-                     value={taskTitle}
+               <div style={{ display: "grid", justifyContent: "end" }}>
+                  <VscEdit size={27} style={{ margin: '1.3% 0 0 1.5%', }}
                      onClick={function () {
-                        // setTaskTitle(props.task.title);
+                        document.querySelector(".hiddenTastArea").style.display = (document.querySelector(".hiddenTastArea").style.display == "none") ? "block" : "none";
                      }}
-                     onBlur={function() {
-                        
-                     }}
-                     onChange={function (event) {
-                        setTaskTitle(event.target.value)
-                     }}
-                  ></input>
-                  <VscCheck size={27} style={{ margin: '1.3% 0 0 1.5%' }}
-                     onClick={function () {
-                        thisTaskEditor()
-                     }
-                     } />
+                  />
+               </div>
+               <p className='greyZone'>{props.task.title}</p>
+               <div className='hiddenTastArea' style={{ display: "none" }}>
+                  <div className='greyZone' >
+
+                     <input maxLength={150} style={{ width: '96%', borderRadius: '3px', padding: '1% ', fontSize: '18px', backgroundColor: 'rgba(27, 54, 68, 0.8)', color: 'white' }}
+                        placeholder="Редактировать задачу"
+                        value={taskTitle}
+                        onChange={function (event) {
+                           setTaskTitle(event.target.value)
+                        }}
+                     ></input>
+                     <VscCheck size={27} style={{ margin: '1.3% 0 0 1.5%' }}
+                        onClick={function () {
+                           thisTaskEditor()
+                        }
+                        } />
+                  </div>
                </div>
                <div className='greyZone' >
                   <p style={{ color: 'slategrey' }} >
@@ -56,23 +44,8 @@ const TaskRedactor = function (props) {
                   </p>
                </div>
             </div>
-            <div >
-               {/* <textarea
-                  value={edited}
-                  onChange={function (event) {
-                     setEdited(event.target.value)
-                  }}
-                  onBlur={function () {
-                     setTaskTitle(edited);
-                     console.log(edited, taskTitle)
-                     thisTaskEditor();
-                  }}
-               ></textarea> 
-               <button onClick={changeTest}></button>*/}
-            </div>
 
-
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
                <VscSignIn size={30} style={{ margin: '0 0 1.5% 1%', }}
                   onClick={() => props.onClose(props.title)}
                />
@@ -87,7 +60,7 @@ const TaskRedactor = function (props) {
                />
             </div>
          </div>
-      </div>
+      </div >
    )
 }
 
